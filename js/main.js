b79aaf1477890af8483ddc57086f3ef7a88b4078
ematsakov <ematsakov@gmail.com>
2015-10-28 17:27:17 +0200

    - delegate test

--- js/main.js
+++ js/main.js
@@ -220,7 +220,7 @@ var qbApp = qbApp || { 'settings': {}, 'behaviors': {} };
 		var $newTag = $newTagForm.find('.new-tag');
 		$newTagForm.show();
 		$newTag.select();
-		$(document).delegate('.ui-content', 'touchmove', false);
+		//$(document).delegate('.ui-content', 'touchmove', false);
 
 		//Load data for submit
 		var activePageId = $.mobile.activePage.attr( "id" );
@@ -260,7 +260,7 @@ var qbApp = qbApp || { 'settings': {}, 'behaviors': {} };
 
 		$newTag.focusout(function(){
 			$newTagForm.hide();
-			$(document).undelegate('.ui-content', 'touchmove', false);
+			//$(document).undelegate('.ui-content', 'touchmove', false);
 			$.mobile.silentScroll(1);
 		})
 	}
