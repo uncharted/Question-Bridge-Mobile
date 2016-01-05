38d69d8bb7ee05c19f44e6d257f299186dd3f31c
ematsakov <ematsakov@gmail.com>
2016-01-05 18:35:01 +0200

    - scrolling fix

--- js/video.capture.js
+++ js/video.capture.js
@@ -81,6 +81,7 @@ function autoCaptureOnCorrectRotate(){
 function videoRecordReturnBack(){
 	$('#take-me-back').find('a.return-back').on('click', function(event) {
 		event.preventDefault();
+		$.mobile.silentScroll(1);
 		if(qbApp.returnPageId.length){
 			$(qbApp.returnPageId).find('div.content-primary').css('visibility', 'visible');
 			$.mobile.changePage(qbApp.returnPageId, {transition: "slidefade", changeHash: false});
@@ -88,6 +89,7 @@ function videoRecordReturnBack(){
 		else {
 			$.mobile.changePage('#page-home', {transition: "slidefade"});
 		}
+		$.mobile.silentScroll(1);
 	});
 }
 
